73088c1cec1bce476499e0a90ee539bd06f69e85
Ulrich Mbouna <msuslegrand@gmail.com>
2023-11-03 23:40:13 +0100

    API Part 19
    - Avancé sur ls microservices avec TCP eT RABBITMQ

---
+++ src/search/search.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+
+@Module({
+  imports: [
+    ConfigModule,
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get('ELASTICSEARCH_NODE'),
+        auth: {
+          username: configService.get('ELASTICSEARCH_USERNAME'),
+          password: configService.get('ELASTICSEARCH_PASSWORD'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [ElasticsearchModule],
+})
+export class SearchModule {}
---
+++ src/subscribers/subscribers.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { SubscribersController } from './subscribers.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [ConfigModule],
+  controllers: [SubscribersController],
+  providers: [
+    {
+      provide: 'SUBSCRIBERS_SERVICE',
+      useFactory: (configService: ConfigService) => {
+        const user = configService.get('RABBITMQ_USER');
+        const password = configService.get('RABBITMQ_PASSWORD');
+        const host = configService.get('RABBITMQ_HOST');
+        const queueName = configService.get('RABBITMQ_QUEUE_NAME');
+
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            host: host,
+            urls: [`amqp://${user}:${password}@${host}`],
+            queue: queueName,
+            noAck: false,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
+export class SubscribersModule {}
