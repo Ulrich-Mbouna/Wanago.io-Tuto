dcd4f5d1741daca81b0632ac8981bf5203a14fe0
Ulrich Mbouna <msuslegrand@gmail.com>
2023-11-04 00:05:56 +0100

    API Part 19
    - Add missing files

---
+++ src/subscribers/subscribers.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
+import { CreateSubscriberDto } from './dto/create-subscriber.dto';
+
+@Controller('subscribers')
+@UseInterceptors(ClassSerializerInterceptor)
+export class SubscribersController {
+  constructor(
+    @Inject('SUBSCRIBERS_SERVICE') private subscribersService: ClientProxy,
+  ) {}
+
+  @Get()
+  @UseGuards(JwtAuthenticationGuard)
+  async getSubscribers() {
+    return this.subscribersService.send(
+      {
+        cmd: 'get-all-subscribers',
+      },
+      '',
+    );
+  }
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  async createPost(@Body() subscriber: CreateSubscriberDto) {
+    return this.subscribersService.send(
+      {
+        cmd: 'add-subscriber',
+      },
+      subscriber,
+    );
+  }
+}
